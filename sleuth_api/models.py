698f115df82e06a4c4e34863eb09ab8f679b2f91
rational-being <overcomer021@gmail.com>
2024-06-29 17:58:09 +0100

    updated models to start implementing user authentication in the api

--- sleuth_api/models.py
+++ sleuth_api/models.py
@@ -1,10 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(256), nullable=False)
-    encoded_filename = db.Column(db.String(256), nullable=True)
-    upload_date = db.Column(db.DateTime, nullable=False)
+    filename = db.Column(db.String(100), nullable=False)
+    encoded_filename = db.Column(db.String(100), nullable=True)
+    upload_date = db.Column(db.DateTime, default=datetime.utcnow)
     message = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('files', lazy=True))
